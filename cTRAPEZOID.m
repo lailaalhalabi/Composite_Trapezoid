64b7c883cb9cabdc8398f570b79a4563dc439f49
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-02-22 18:55:49 -0500

    Update cTRAPEZOID.m

--- cTRAPEZOID.m
+++ cTRAPEZOID.m
@@ -1,7 +1,7 @@
 function I = cTRAPEZOID(f,a,b,m) 
   
-  #Composite trapezoid rule.
-  
+  # Composite trapezoid rule.
+  # a & b  are the start and end points of the integral and m is the sub-intercals
   format long
   h = (b-a)/m;
   x = linspace(a,b,m+1);
@@ -12,4 +12,13 @@ function I = cTRAPEZOID(f,a,b,m)
   I = h/2*(2*sum(f(x))-f(a)-f(b))
   fprintf('\n')
   
+  # Example
+  # I = cTRAPEZOID(@(x)log(x),1,2,4)
+  
+  # WE GET:
+  # LONG FORMAT
+  # I =    3.836995094094423e-01
+  # SHORT FORMAT
+  # I =  0.38370
+
 endfunction
